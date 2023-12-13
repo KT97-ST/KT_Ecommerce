5cea765ebc1b8d142b8d66cfaadae1ee8404c268
KOCoders <74465408+KOCoders@users.noreply.github.com>
2023-12-13 22:10:46 +0700

    Product list and search

--- KT_EcommerceMVC/Controllers/ProductController.cs
+++ KT_EcommerceMVC/Controllers/ProductController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KT_EcommerceMVC.Data;
+using KT_EcommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KT_EcommerceMVC.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly KtEcommerceContext _context;
+        public ProductController(KtEcommerceContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index(int? category)
         {
-            return View();
+            var products = _context.Products.AsQueryable();
+            if (category.HasValue)
+            {
+                products = products.Where(p=>p.CategoryId == category.Value);
+            }
+            var result = products.Select(p => new ProductsVM { 
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                UnitPrice = p.UnitPrice ?? 0,
+                Image = p.Image ?? "",
+                Description = p.Description ?? "",
+                CategoryName = p.Category.CategoryName
+
+            });
+            return View(result);
+        }
+
+        public IActionResult Search(string? query)
+        {
+            var products = _context.Products.AsQueryable();
+            if (query != null)
+            {
+                products = products.Where(p => p.ProductName.Contains(query));
+            }
+            var result = products.Select(p => new ProductsVM
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                UnitPrice = p.UnitPrice ?? 0,
+                Image = p.Image ?? "",
+                Description = p.Description ?? "",
+                CategoryName = p.Category.CategoryName
+
+            });
+            return View(result);
         }
     }
 }
---
+++ KT_EcommerceMVC/ViewModels/ProductsVM.cs
@@ -0,0 +1,17 @@
+﻿namespace KT_EcommerceMVC.ViewModels
+{
+    public class ProductsVM
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Image { get; set; }
+    
+        public double UnitPrice { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+
+
+    }
+}
